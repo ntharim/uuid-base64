c63e2fb4f8298bffb37b4ce59da9fd3b20128328
Nathan Tran <ntran013@gmail.com>
2014-09-29 03:49:10 +1000

    Release 0.0.2

--- test/uuid-base64.js
+++ test/uuid-base64.js
@@ -3,7 +3,7 @@ var toBase64 = require('..');
 
 describe('UUID to Base64', function () {
   it ('should compress UUID to 22 characters', function () {
-    var id = toBase64('0e8c72e5-463f-446e-bcf2-695274d55940');
+    var id = toBase64('38cb87b5-b23a-481e-8373-b238e5e864c4');
     assert(id.length == 22);
   });
 });
